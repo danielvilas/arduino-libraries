98bc3bc8e4c5104cba1e3cd0b9c372e75f5b2d96
Daniel Vilas <daniel.vilas@gmail.com>
2014-08-03 18:30:11 +0200

    Comiezo soporte a varios canales

--- CHAL74hc595/chal74hc595.h
+++ CHAL74hc595/chal74hc595.h
@@ -11,7 +11,7 @@
 
 class Chal74hc595 {
 private:
-    int _pinDs; // Data Serial Pin (DS) 14 in IC
+    int* _pinDs; // Data Serial Pin (DS) 14 in IC
     int _pinStCp; // Storage Clock Pin (ST_CP) 12 in IC
     int _pinShCp; // Shift Register Clok (SH_CP) 11 in IC
     int _channels;
@@ -19,7 +19,7 @@ private:
     
 public:
     Chal74hc595(int channels, int icsPerChannel);
-    void setup(int _ds, int _sh, int _st);
+    void setup(int* _ds, int _sh, int _st);
     void shift(int* data);
 };
 
